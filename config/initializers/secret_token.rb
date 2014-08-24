79f0d658c530943c1dd92310add8494daed47407
Amanda Dillon <amandacdillon@gmail.com>
2014-08-24 16:31:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LookiluWeb::Application.config.secret_key_base = 'fe0368ea10e1b8f98fdf639f9e49a047d97d4351853de0a3f5d6654d8a8eb605fd2ad1ca3b33ae03cfd24231fde832520edc31cdf8f27e818690d2695762de4f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LookiluWeb::Application.config.session_store :cookie_store, key: '_lookilu_web_session'
