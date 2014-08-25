beb29fe278c8074c3d47e4a6828b33a26744421a
Amanda Dillon <amandacdillon@gmail.com>
2014-08-24 21:21:02 -0700

    Update RSpec file to include title tests

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Pages" do
  
+  let(:base_title) { "Lookilu" }
+
   describe "Home page" do
     
     it "should have the content 'Lookilu'" do
@@ -16,6 +18,11 @@ describe "Pages" do
       visit '/pages/about'
       expect(page).to have_content('About Lookilu')
     end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
+    end
   end
 
   describe "Contact page" do
@@ -24,6 +31,11 @@ describe "Pages" do
   		visit '/pages/contact'
   		expect(page).to have_content('Get In Touch')
   	end
+
+  	it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact Us")
+    end
   end
 
 end
